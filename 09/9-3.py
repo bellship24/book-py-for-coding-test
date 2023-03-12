245932fe352ad746a23912e00948ab1aad139c3d
bellship24 <bellship24@gmail.com>
2023-03-13 00:55:20 +0900

    9. shortest path : concept : floyd_warshall

---
+++ 09/9-3.py
@@ -0,0 +1,56 @@
+### 플로이드 워셜 알고리즘 소스코드
+
+INF = int(1e9)  # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수 및 간선의 개수를 입력받기
+n = int(input())
+m = int(input())
+
+# 2차원 리스트(그래프 표현)를 만들고 모든 값을 무한으로 초기화, 인덱스를 노드 번호로 사용하기 위해 n + 1을 사용함. 즉, 인덱스 0은 사용하지 않음.
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
+for _ in range(m):
+    # A에서 B로 가는 비용은 C라고 설정
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 수행된 결과를 출력
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+        if graph[a][b] == INF:
+            print("INFINITY", end=" ")
+        # 도달할 수 있는 경우, 거리를 출력
+        else:
+            print(graph[a][b], end=" ")
+    print()
+
+'''
+4
+7
+1 2 4
+1 4 6
+2 1 3
+2 3 7
+3 1 5
+3 4 4
+4 3 2
+
+0 4 8 6 
+3 0 7 9 
+5 9 0 4 
+7 11 2 0 
+'''
