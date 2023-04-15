f15d16801bcce63d78d71bfbdfb94408b26eab00
bellship24 <bellship24@gmail.com>
2023-04-16 00:36:28 +0900

    10. graph theory : concept
    10-4.py
    - 서로소 집합을 활용한 사이클 판별
    
    10-5-review.py
    - 크루스칼 알고리즘
    
    10-6.py
    - 위상 정렬

---
+++ 10/10-4.py
@@ -0,0 +1,55 @@
+# 서로소 집합을 활용한 사이클 판별 소스코드
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, x)
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선(union 연산)의 개수 입력받기
+v, e = map(int, input().split())
+
+# 부모 테이블에서 부모를 자기 자신으로 초기화
+parent = [i for i in range(v + 1)]
+
+# 사이클 발생 여부
+cycle = False
+
+for i in range(e):
+    a, b = map(int, input().split())
+
+    # 사이클이 발생한 경우 종료
+    if parent[a] == parent[b]:
+        cycle = True
+        break
+    # 사이클이 발생하지 않았다면 합집합(union) 수행
+    else:
+        union_parent(parent, a, b)
+    
+
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
+
+
+'''
+Intput
+3 3
+1 2
+1 3
+2 3
+
+Output
+사이클이 발생했습니다.
+'''
